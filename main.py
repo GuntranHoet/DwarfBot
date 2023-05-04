55c07ad811d05a0ddf65757e1bf2cdd618b12f3e
GunTheHuman <guntranonline+github@gmail.com>
2023-05-04 21:40:15 +0100

    Updated bot to user newer discord API + moved quotes and triggers into a data folder

--- main.py
+++ main.py
@@ -1,44 +1,112 @@
 ##### IMPORTS #####
+import os
 import random
+import time
+import asyncio
 
 import discord
 from discord.ext import commands
 
-from keep_alive import  keep_alive
+from keep_alive import keep_alive
 
 ##### GLOBALS #####
-TOKEN = 'YOUR_TOKEN_HERE'
+TOKEN = 'YOUR_TOKEN_HERE' # <<---------------- ENTER YOUR TOKEN HERE ------------- <<
 CMD = '!'
-QUOTES = open('quotes.txt').read().splitlines()
-TRIGGERS = open('triggers.txt').read().splitlines()
+ALLOW_TTS = False
+QUOTES = open('data/quotes.txt').read().splitlines()
+TRIGGERS = open('data/triggers.txt').read().splitlines()
+
+DEFAULT_ACTIVITY = discord.Activity(type=discord.ActivityType.playing,
+                                    name="Deep Rock Galacitc")
+
+##### HELPERS (cooldown) #####
+COOLDOWN_SEC = 60 * 5
+LAST_COOLDOWN_EXPIRE_TIME = 0
+
+
+def IsCooldownFinished():
+    now = int(time.time())
+
+    ok = (now >= LAST_COOLDOWN_EXPIRE_TIME)
+    print(">", "now:", now, "cooldown_expire:", LAST_COOLDOWN_EXPIRE_TIME,
+          "dif:", now - LAST_COOLDOWN_EXPIRE_TIME, "ok:", ok)
+    return ok
+
+
+def BeginCooldown():
+    global LAST_COOLDOWN_EXPIRE_TIME
+    LAST_COOLDOWN_EXPIRE_TIME = int(time.time()) + COOLDOWN_SEC
+
+
+def GetCooldownTminusSec():
+    return LAST_COOLDOWN_EXPIRE_TIME - int(time.time())
+
 
 ##### DISCORD #####
-client = commands.Bot(command_prefix = CMD)
+#bot = commands.Bot(command_prefix=CMD)
+#bot = discord.Client(intents=discord.Intents.default())
+##intents = discord.Intents.default()
+##intents.typing = True
+##intents.presences = True
+##intents.members = True
+##intents.messages = True
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix=CMD, intents=intents)
+
 
 # READY #
-@client.event
+@bot.event
 async def on_ready():
     print('DwarfBot ready!')
+    await bot.change_presence(activity=DEFAULT_ACTIVITY,
+                              status=discord.Status.online)
+
 
 # ON MESSAGE #
-@client.event
+@bot.event
 async def on_message(message):
-		#don't trigger on messages sent by this bot
-		if message.author == client.user:
-			return
-		
-		#ignore messages containing '!' to prevent triggering on commands
-		if CMD in message.content:
-			return
+    if message.author == bot.user:
+        return
 
-		#always convert to lower case for convenience in the checks below
-		lowerContent = message.content.lower()
+    #if CMD in message.content:
+    #	return
 
-		for x in TRIGGERS:
-			if x in lowerContent:
-				await message.channel.send( random.choice(QUOTES) )
-				break
+    if not IsCooldownFinished():
+        return
+
+    lowerContent = message.content.lower()
+    print("trigger msg: " + message.content)
+
+    for x in TRIGGERS:
+        if x in lowerContent:
+            print("creating message...")
+            await message.channel.send(random.choice(QUOTES), tts=ALLOW_TTS)
+            BeginCooldown()
+            bot.loop.create_task(cooldown_update())
+            break
+
+
+##### CUSTOM (discord) #####
+async def cooldown_update():
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        if IsCooldownFinished():
+            await bot.change_presence(activity=DEFAULT_ACTIVITY,
+                                      status=discord.Status.online)
+            return
+        else:
+            tminus = GetCooldownTminusSec()
+            await bot.change_presence(activity=discord.Activity(
+                type=discord.ActivityType.watching,
+                name=f"cooldown: {tminus}s"),
+                                      status=discord.Status.do_not_disturb)
+            #await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.custom, name=f"Cooldown: -{tminus}s"), status=discord.Status.do_not_disturb)
+
+            #sleep and redo
+            await asyncio.sleep(5)
+
 
 ##### CORE #####
 keep_alive()
-client.run(TOKEN)
+#bot.loop.create_task(cooldown_update())
+bot.run(TOKEN)
